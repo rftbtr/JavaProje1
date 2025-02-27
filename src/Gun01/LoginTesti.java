60453359032e2f8b9a72203d78ecf140e6bc9ddf
rftbtr <rifatbatir@gmail.com>
2025-02-27 20:34:37 +0300

    tekrar sepet testi yapıldı

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -4,11 +4,15 @@ public class LoginTesti {
     public static void main(String[] args) {
 
         // Bu bölüm Login testi için, yeni şifre modeline göre yazıldı
-        System.out.println("Yeni Login Testi ");
+        System.out.println("Yeni Login değiştirdim Testi ");
 
         // çok satır yazdım, çünkü test senaryosu uzun olabilir
         System.out.println("Login Testi 1");
 
         System.out.println("Sepet testi yazildi bitirildi,3 günlük kod yazild1");
+
+        System.out.println("Login Testi 2");
+
+        System.out.println("Login Testi 3 yedi gün uğraltım anam ağladı");
     }
 }
