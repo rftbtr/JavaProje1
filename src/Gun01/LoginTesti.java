a563072fb22b6b628dcecd0e99f1bbbb5ecabcc7
rftbtr <rifatbatir@gmail.com>
2025-02-27 20:24:20 +0300

    Login testi aşama 1 (Jirada gelen taskdaki başlık)

---
+++ src/Gun01/LoginTesti.java
@@ -0,0 +1,14 @@
+package Gun01;
+
+public class LoginTesti {
+    public static void main(String[] args) {
+
+        // Bu bölüm Login testi için, yeni şifre modeline göre yazıldı
+        System.out.println("Yeni Login Testi ");
+
+        // çok satır yazdım, çünkü test senaryosu uzun olabilir
+        System.out.println("Login Testi 1");
+
+        
+    }
+}
